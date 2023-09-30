861f04f36ee36b83808cf9bd90b3db3a42f392ef
ivany <ivan.yakimishin.00@mail.ru>
2023-09-30 15:51:24 +0300

    UpdateActivity.kt showKeyBoard added

--- app/src/main/java/com/example/notesapp/view/activities/UpdateActivity.kt
+++ app/src/main/java/com/example/notesapp/view/activities/UpdateActivity.kt
@@ -1,19 +1,24 @@
 package com.example.notesapp.view.activities
 
+import android.content.Context
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import android.widget.LinearLayout
 import com.example.notesapp.R
 import com.example.notesapp.model.dao.NotesDao
 import com.example.notesapp.model.database.NotesDatabase
+import kotlinx.android.synthetic.main.activity_update.*
 
 class UpdateActivity : AppCompatActivity() {
     private lateinit var notesDao: NotesDao
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_update)
+
 
         val itemId = intent.extras?.getString("item_id")
 
@@ -23,6 +28,12 @@ class UpdateActivity : AppCompatActivity() {
 
         val editTextTag = findViewById<EditText>(R.id.update_editTextTag)
         val editTextText = findViewById<EditText>(R.id.update_editTextText)
+        val linearLayout = findViewById<LinearLayout>(R.id.update_linearLayout)
+
+        linearLayout.setOnClickListener {
+            editTextText.requestFocus()
+            showKeyBoard()
+        }
 
         AsyncTask.execute {
             val note = notesDao.getNoteById(itemId)
@@ -40,5 +51,10 @@ class UpdateActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    fun showKeyBoard() {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(update_editTextText, InputMethodManager.SHOW_IMPLICIT)
     }
 }
